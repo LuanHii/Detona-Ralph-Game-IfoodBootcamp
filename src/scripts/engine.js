05d1dc25d63883d99bfca5ea7667fa696fd403fb
LuanHii <luanhiivitor@gmail.com>
2024-01-05 00:58:15 -0300

    game loop created

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,56 @@
-alert("hello world");
+const state = {
+    view: {
+        squares: document.querySelectorAll(".squares"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+
+    },
+    value: {
+        timerId: null,
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        
+
+
+
+    },
+};
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.value.hitPosition = randomSquare.id;
+}
+
+function moveEnemy(){
+    state.value.timerId = setInterval(randomSquare, state.value.gameVelocity);
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => { 
+        square.addEventListener("mousedown", ()=> {
+            if (square.id === state.value.hitPosition) {
+                state.value.result++;
+                state.view.score.textContent = state.value.result;
+                state.value.hitPosition = null;
+            }
+                })
+    });
+}
+
+function initialize() {
+    moveEnemy();
+    addListenerHitBox();
+}
+
+
+initialize();
